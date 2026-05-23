7ca35097545946c1b3c5cc6bd845e669518cf9d8
Adjei Yajim Randy <adjeiyajimrandy143@gmail.com>
2026-05-23 18:11:13 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function revealText() {
+  // Hide the name
+  document.getElementById("nameBox").style.display = "none";
+
+  // Show the message
+  const wishBox = document.getElementById("wishBox");
+  const birthdayText = document.getElementById("BText");
+
+  wishBox.classList.remove("hidden");
+
+  // Add animation to the text
+  BText.classList.add("b-animate");
+}
+ 
